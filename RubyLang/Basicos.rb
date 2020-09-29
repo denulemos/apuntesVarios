d966e719be0f40daf5ad71c9591a95a499f32b5e
Denisse Lemos <denu.a.lemos@gmail.com>
2020-09-29 16:57:28 -0300

    nuevo

---
+++ RubyLang/Basicos.rb
@@ -0,0 +1,46 @@
+# ------------------------Imprimir Hello World en consola------------------------
+puts "Hello World" #Todos los Strings deben estar entre comillas
+print "Hello World" 
+
+#puts -> Agrega salto de linea
+#print -> Todo de corrido
+
+#------------------------Comentarios multilinea------------------------
+
+=begin
+hola
+soy
+un
+comentario
+=end
+
+#------------------------Variables------------------------
+x = 8
+e,a,t = 11,12,44 #Varias asignaciones en paralelo
+
+#Constantes deben estar escritos con mayuscula
+MyConstante = 42
+
+#El tipo de dato de la variable se define automaticamente
+
+#Para concatenar variables
+puts "Mi constante tiene un valor de #{MyConstante}"
+
+#------------------------Operaciones matematicas------------------------
+
+x= 5
+y=2
+#Si la operacion es entre 2 int, el resultado sera un int.
+
+#Suma
+puts x+y 
+#Resta
+puts x-y 
+#Multiplicacion
+puts x*y
+#Division
+puts x/y
+#Resto
+puts x%y
+#Exponente
+puts x**y
---
+++ RubyLang/README.md
@@ -0,0 +1,10 @@
+# Ruby
+
+Repo hecho en base a Sololearn.
+
+## ¿Qué es Ruby?
+
+* Es un lenguaje orientado a objetos.
+* Es popular gracias a **Ruby on Rails** framework.
+* Segun el creador, en Ruby, todo es un objeto.
+
